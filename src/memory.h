a0578aa5df0d1bb2bd581725630e9db483d4c601
Trevor Pickrell <tcpickrell93@gmail.com>
2022-03-13 00:43:24 -0500

    Add ram512, ram4k, ram16k

--- src/memory.h
+++ src/memory.h
@@ -78,5 +78,49 @@ private:
     RAM8 ram8_7_ {};
 };
 
+class RAM512 {
+public:
+    byte2 Read(const bit9& address);
+    void Write(const bit9& address, const byte2& in, const bit& load);
+
+private:
+    RAM64 ram64_0_ {};
+    RAM64 ram64_1_ {};
+    RAM64 ram64_2_ {};
+    RAM64 ram64_3_ {};
+    RAM64 ram64_4_ {};
+    RAM64 ram64_5_ {};
+    RAM64 ram64_6_ {};
+    RAM64 ram64_7_ {};
+};
+
+class RAM4k {
+public:
+    byte2 Read(const bit12& address);
+    void Write(const bit12& address, const byte2& in, const bit& load);
+
+private:
+    RAM512 ram512_0_ {};
+    RAM512 ram512_1_ {};
+    RAM512 ram512_2_ {};
+    RAM512 ram512_3_ {};
+    RAM512 ram512_4_ {};
+    RAM512 ram512_5_ {};
+    RAM512 ram512_6_ {};
+    RAM512 ram512_7_ {};
+};
+
+class RAM16k {
+public:
+    byte2 Read(const bit14& address);
+    void Write(const bit14& address, const byte2& in, const bit& load);
+
+private:
+    RAM4k ram4k_0_ {};
+    RAM4k ram4k_1_ {};
+    RAM4k ram4k_2_ {};
+    RAM4k ram4k_3_ {};
+};
+
 #endif // MEMORY_H
 
