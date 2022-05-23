f2f24703781cc54f4a35f1a09a31874f347a7473
Trevor Pickrell <tcpickrell93@gmail.com>
2022-05-23 00:32:20 -0400

    Add comments

--- src/cpu.h
+++ src/cpu.h
@@ -13,15 +13,18 @@ public:
                byte2& outM,                 // output value to write to memory in addressM
                bit& writeM,                 // write enable bit for output to be written
                byte2& addressM);            // address in which we want to write to memory
+   byte2 Get_reg_A();
+   byte2 Get_reg_D();
+
 private:
    bit Jump(const bit& j1,
             const bit& j2, 
             const bit& j3, 
             const bit& zr, 
-            const bit& ng);   // internal function used to determine if jump is requested
-   Register16 reg_A_ {};                     // address register
-   Register16 reg_D_ {};                     // data register
-   ProgramCounter pc_ {};                    // address of next instruction
+            const bit& ng);      // internal function used to determine if jump is requested
+   Register16 reg_A_ {};         // address register
+   Register16 reg_D_ {};         // data register
+   ProgramCounter pc_ {};        // address of next instruction
 };
 
 #endif // CPU_H
