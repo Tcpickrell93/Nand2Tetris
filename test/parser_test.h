26b804b99ed9ac4c7f86cff595b3fe31e37c1359
Trevor Pickrell <tcpickrell93@gmail.com>
2022-05-30 21:53:52 -0400

    Add more parser tests

--- test/parser_test.h
+++ test/parser_test.h
@@ -31,3 +31,72 @@ TEST_F(ParserTest, Advance_test) {
     std::string expected { "@2" };
     ASSERT_EQ(result, expected);
 }
+
+TEST_F(ParserTest, Instruction_type_test) {
+    std::string line { "@1" };
+    instruction_type result { parser.Instruction_type(line) };
+    instruction_type expected { A_INSTRUCTION };
+    ASSERT_EQ(result, expected);
+    line = "(LOOP)";
+    result = parser.Instruction_type(line);
+    expected = L_INSTRUCTION;
+    ASSERT_EQ(result, expected);
+    line = "D=D+A";
+    result = parser.Instruction_type(line);
+    expected = C_INSTRUCTION;
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(ParserTest, Symbol_test) {
+    std::string line { "@147892" };
+    instruction_type instruction { A_INSTRUCTION };
+    std::string result { parser.Symbol(line, instruction) };
+    std::string expected { "147892" };
+    ASSERT_EQ(result, expected);
+    line = "(LOOP)";
+    instruction = L_INSTRUCTION;
+    result = parser.Symbol(line, instruction);
+    expected = "LOOP";
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(ParserTest, Dest_test) {
+    std::string line { "D=D+A;JMP" };
+    std::string result { parser.Dest(line) };
+    std::string expected { "D" };
+    ASSERT_EQ(result, expected);
+    line = "ADM=D+A;JMP";
+    result = parser.Dest(line);
+    expected = "ADM";
+    ASSERT_EQ(result, expected);
+}   
+
+TEST_F(ParserTest, Comp_test) {
+    std::string line { "D=D+A;JMP" };
+    std::string result { parser.Comp(line) };
+    std::string expected { "D+A" };
+    ASSERT_EQ(result, expected);
+    line = "D=A;JMP";
+    result = parser.Comp(line);
+    expected = "A";
+    ASSERT_EQ(result, expected);
+    line = "D=1;JMP";
+    result = parser.Comp(line);
+    expected = "1";
+    ASSERT_EQ(result, expected);
+}   
+
+TEST_F(ParserTest, Jump_test) {
+    std::string line { "D=D+A;JMP" };
+    std::string result { parser.Jump(line) };
+    std::string expected { "JMP" };
+    ASSERT_EQ(result, expected);
+    line = "D=D+A";
+    result = parser.Jump(line);
+    expected = "null";
+    ASSERT_EQ(result, expected);
+    line = "@123";
+    result = parser.Jump(line);
+    expected = "null";
+    ASSERT_EQ(result, expected);
+}   
