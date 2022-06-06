68ec0623cacd1b0895d88901090c7bf3367d4252
Trevor Pickrell <tcpickrell93@gmail.com>
2022-06-05 20:08:49 -0400

    Add test for A instructions

--- test/assembler_test.h
+++ test/assembler_test.h
@@ -18,8 +18,8 @@ class AssemblerTest: public ::testing::Test {
         std::map<std::string, int> symbol_table {
             {"STOP", 4},
             {"LOOP", 8},    
+            {"sum", 17},
         };
-        std::string symbol { "STOP" };
 };
 
 TEST_F(AssemblerTest, Process_C_instruction_test) {
@@ -30,6 +30,7 @@ TEST_F(AssemblerTest, Process_C_instruction_test) {
 }
 
 TEST_F(AssemblerTest, Process_L_instruction_test) {
+    std::string symbol { "STOP" };
     std::string result { Assembler::Process_L_instruction(symbol, symbol_table) };
     byte2 expected_bin { 0b0000'0000'0000'0100u };
     std::string expected { expected_bin.to_string() };
@@ -37,6 +38,28 @@ TEST_F(AssemblerTest, Process_L_instruction_test) {
     symbol = "LOOP";
     result = Assembler::Process_L_instruction(symbol, symbol_table);
     expected_bin = 0b0000'0000'0000'1000u;
+    expected = expected_bin.to_string();
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(AssemblerTest, Process_A_instruction_test) {
+    int current_available_address { 18 };
+    std::string symbol { "8" };
+    std::string result { Assembler::Process_A_instruction(symbol, symbol_table, current_available_address) };
+    byte2 expected_bin { 0b0000'0000'0000'1000u };
+    std::string expected { expected_bin.to_string() };
+    ASSERT_EQ(result, expected);
+    symbol = "diff";
+    result = Assembler::Process_A_instruction(symbol, symbol_table, current_available_address);
+    expected_bin = 0b0000'0000'0001'0010u;
+    expected = expected_bin.to_string();
+    ASSERT_EQ(result, expected);
+    int symbol_table_entry_result { symbol_table["diff"] };
+    int symbol_table_entry_expected { 18 };
+    ASSERT_EQ(symbol_table_entry_result, symbol_table_entry_expected);
+    symbol = "sum";
+    result = Assembler::Process_A_instruction(symbol, symbol_table, current_available_address);
+    expected_bin = 0b0000'0000'0001'0001u;
     expected = expected_bin.to_string();
     ASSERT_EQ(result, expected);
 }
