5b4d317b5d21fcf2fa57282c91be59b284ee9218
Trevor Pickrell <tcpickrell93@gmail.com>
2022-07-10 15:22:11 -0400

    Conver to new bits

--- test/logic_gates_test.h
+++ test/logic_gates_test.h
@@ -5,270 +5,252 @@
 
 class LogicGatesTest : public ::testing::Test {
     protected:
-        bit a{ 0b0u };
-        bit b{ 0b0u };
-        bit sel{ 0b0u };
+        bit1_u a { 0 };
+        bit1_u b { 0 };
+        bit1_u sel { 0 };
 
-        bit2 sel2{ 0b00u };
+        bit2_u sel2 { 0 };
         
-        bit3 sel3{ 0b00u };
+        bit3_u sel3 { 0 };
 
-        byte a8{ 0b0000'0000u };
+        byte1_u a8 { 0 };
 
-        byte2 a16{ 0b0000'0000'0000'0000 };
-        byte2 b16{ 0b1111'1111'1111'1111 };
-        byte2 c16{ 0b1111'1111'0000'0000 };
-        byte2 d16{ 0b0000'0000'1111'1111 };
-        byte2 e16{ 0b1111'0000'0000'0000 };
-        byte2 f16{ 0b0000'1111'0000'0000 };
-        byte2 g16{ 0b0000'0000'1111'0000 };
-        byte2 h16{ 0b0000'0000'0000'1111 };
+        byte2_u a16 { 0 };
+        byte2_u b16 { 65535 };
+        byte2_u c16 { 65280 };
+        byte2_u d16 { 255 };
+        byte2_u e16 { 61440 };
+        byte2_u f16 { 3840 };
+        byte2_u g16 { 240 };
+        byte2_u h16 { 15 };
 };
 
 TEST_F(LogicGatesTest, Nand) {
     // a=0, b=0
-    ASSERT_TRUE(Nand(a, b)[0]);
+    ASSERT_TRUE(Nand(a, b).value);
 
     // a=1, b=0
-    a.flip();
-    ASSERT_TRUE(Nand(a, b)[0]);
+    a.value = 1;
+    ASSERT_TRUE(Nand(a, b).value);
 
     // a=1, b=1
-    b.flip();
-    ASSERT_FALSE(Nand(a, b)[0]);
+    b.value = 1;
+    ASSERT_FALSE(Nand(a, b).value);
 
     // a=0, b=1
-    a.flip();
-    ASSERT_TRUE(Nand(a, b)[0]);
+    a.value = 0;
+    ASSERT_TRUE(Nand(a, b).value);
 }
     
 TEST_F(LogicGatesTest, Not) {
     // a=0
-    ASSERT_TRUE(Not(a)[0]);
+    ASSERT_TRUE(Not(a).value);
 
     // a=1
-    a.flip();
-    ASSERT_FALSE(Not(a)[0]);
+    a.value = 1;
+    ASSERT_FALSE(Not(a).value);
 }
 
 TEST_F(LogicGatesTest, And) {
     // a=0, b=0
-    ASSERT_FALSE(And(a, b)[0]);
+    ASSERT_FALSE(And(a, b).value);
 
     // a=1, b=0
-    a.flip();
-    ASSERT_FALSE(And(a, b)[0]);
+    a.value = 1;
+    ASSERT_FALSE(And(a, b).value);
 
     // a=1, b=1
-    b.flip();
-    ASSERT_TRUE(And(a, b)[0]);
+    b.value = 1;
+    ASSERT_TRUE(And(a, b).value);
 
     // a=0, b=1
-    a.flip();
-    ASSERT_FALSE(And(a, b)[0]);
+    a.value = 0;
+    ASSERT_FALSE(And(a, b).value);
 }
 
 TEST_F(LogicGatesTest, Or) {
     // a=0, b=0
-    ASSERT_FALSE(Or(a, b)[0]);
+    ASSERT_FALSE(Or(a, b).value);
 
     // a=1, b=0
-    a.flip();
-    ASSERT_TRUE(Or(a, b)[0]);
+    a.value = 1;;
+    ASSERT_TRUE(Or(a, b).value);
 
     // a=1, b=1
-    b.flip();
-    ASSERT_TRUE(Or(a, b)[0]);
+    b.value = 1;
+    ASSERT_TRUE(Or(a, b).value);
 
     // a=0, b=1
-    a.flip();
-    ASSERT_TRUE(Or(a, b)[0]);
+    a.value = 0;
+    ASSERT_TRUE(Or(a, b).value);
 }
 
 TEST_F(LogicGatesTest, Xor) {
     // a=0, b=0
-    ASSERT_FALSE(Xor(a, b)[0]);
+    ASSERT_FALSE(Xor(a, b).value);
 
     // a=1, b=0
-    a.flip();
-    ASSERT_TRUE(Xor(a, b)[0]);
+    a.value = 1;;
+    ASSERT_TRUE(Xor(a, b).value);
 
     // a=1, b=1
-    b.flip();
-    ASSERT_FALSE(Xor(a, b)[0]);
+    b.value = 1;
+    ASSERT_FALSE(Xor(a, b).value);
 
     // a=0, b=1
-    a.flip();
-    ASSERT_TRUE(Xor(a, b)[0]);
+    a.value = 0;
+    ASSERT_TRUE(Xor(a, b).value);
 }
 
 TEST_F(LogicGatesTest, Mux) {
     // a=0, b=0, sel=0
-    ASSERT_FALSE(Mux(a, b, sel)[0]);
+    ASSERT_FALSE(Mux(a, b, sel).value);
     
     // a=0, b=0, sel=1
-    sel.flip();
-    ASSERT_FALSE(Mux(a, b, sel)[0]);
+    sel.value = 1;
+    ASSERT_FALSE(Mux(a, b, sel).value);
 
     // a=1, b=0, sel=1
-    a.flip();
-    ASSERT_FALSE(Mux(a, b, sel)[0]);
+    a.value = 1;;
+    ASSERT_FALSE(Mux(a, b, sel).value);
 
     // a=1, b=0, sel=0
-    sel.flip();
-    ASSERT_TRUE(Mux(a, b, sel)[0]);
+    sel.value = 0;
+    ASSERT_TRUE(Mux(a, b, sel).value);
 
     // a=1, b=1, sel=0
-    b.flip();
-    ASSERT_TRUE(Mux(a, b, sel)[0]);
+    b.value = 1;
+    ASSERT_TRUE(Mux(a, b, sel).value);
 
     // a=1, b=1, sel=1
-    sel.flip();
-    ASSERT_TRUE(Mux(a, b, sel)[0]);
+    sel.value = 1;
+    ASSERT_TRUE(Mux(a, b, sel).value);
 
     // a=0, b=1, sel=1
-    a.flip();
-    ASSERT_TRUE(Mux(a, b, sel)[0]);
+    a.value = 0;
+    ASSERT_TRUE(Mux(a, b, sel).value);
 
     // a=0, b=1, sel=0
-    sel.flip();
-    ASSERT_FALSE(Mux(a, b, sel)[0]);
+    sel.value = 0;
+    ASSERT_FALSE(Mux(a, b, sel).value);
 }
 
 TEST_F(LogicGatesTest, DMux) {
     // a=0, sel=0
-    bit2 result{ DMux(a, sel) };
-    ASSERT_FALSE(result[0]);
-    ASSERT_FALSE(result[1]);
+    bit2_u result{ DMux(a, sel) };
+    ASSERT_FALSE(result.bit2_s.val0);
+    ASSERT_FALSE(result.bit2_s.val1);
     
     // a=0, sel=1
-    sel.flip();
+    sel.value = 1;
     result = DMux(a, sel); 
-    ASSERT_FALSE(result[0]);
-    ASSERT_FALSE(result[1]);
+    ASSERT_FALSE(result.bit2_s.val0);
+    ASSERT_FALSE(result.bit2_s.val1);
 
     // a=1, sel=1
-    a.flip();
+    a.value = 1;;
     result = DMux(a, sel); 
-    ASSERT_FALSE(result[0]);
-    ASSERT_TRUE(result[1]);
+    ASSERT_FALSE(result.bit2_s.val0);
+    ASSERT_TRUE(result.bit2_s.val1);
 
     // a=1, sel=0
-    sel.flip();
+    sel.value = 0;
     result = DMux(a, sel); 
-    ASSERT_TRUE(result[0]);
-    ASSERT_FALSE(result[1]);
+    ASSERT_TRUE(result.bit2_s.val0);
+    ASSERT_FALSE(result.bit2_s.val1);
 }
 
 TEST_F(LogicGatesTest, AndMuxOr) {
     // a=1, b=0, sel=1
-    sel.flip();
-    a.flip();
-    ASSERT_EQ(AndMuxOr(a, b, sel)[0], Or(a, b)[0]);
+    sel.value = 1;
+    a.value = 1;;
+    ASSERT_EQ(AndMuxOr(a, b, sel).value, Or(a, b).value);
 
     // a=1, b=0, sel=0
-    sel.flip();
-    ASSERT_EQ(AndMuxOr(a, b, sel)[0], And(a, b)[0]);
+    sel.value = 0;
+    ASSERT_EQ(AndMuxOr(a, b, sel).value, And(a, b).value);
 }
 
 TEST_F(LogicGatesTest, Not16) {
-    byte2 result{ Not16(a16) };
-    for (int i = 0; i < 16; i++) {
-        ASSERT_TRUE(result[i]);
-    }
+    byte2_u result{ Not16(a16) };
+    ASSERT_EQ(result.value, b16.value);
 }
 
 TEST_F(LogicGatesTest, And16) {
     // a16=0000'0000'0000'0000
     // b16=0000'0000'0000'0000
-    b16.flip();
-    byte2 result{ And16(a16, b16) };
-    for (int i = 0; i < 16; i++) {
-        ASSERT_FALSE(result[i]);
-    }
+    b16.value = 0;
+    byte2_u result{ And16(a16, b16) };
+    ASSERT_FALSE(result.value);
 
     // a16=1111'1111'1111'1111
     // b16=0000'0000'0000'0000
-    a16.flip();
+    a16.value = 65535;
     result = And16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_FALSE(result[i]);
-    }
+    ASSERT_FALSE(result.value);
 
     // a16=1111'1111'1111'1111
     // b16=1111'1111'1111'1111
-    b16.flip();
+    b16.value = 65535;
     result = And16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_TRUE(result[i]);
-    }
+    ASSERT_EQ(result.value, 65535);
 
     // a16=0000'0000'0000'0000
     // b16=1111'1111'1111'1111
-    a16.flip();
+    a16.value = 0;
     result = And16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_FALSE(result[i]);
-    }
+    ASSERT_FALSE(result.value);
 }
 
 TEST_F(LogicGatesTest, Or16) {
     // a16=0000'0000'0000'0000
     // b16=0000'0000'0000'0000
-    b16.flip();
-    byte2 result{ Or16(a16, b16) };
-    for (int i = 0; i < 16; i++) {
-        ASSERT_FALSE(result[i]);
-    }
+    b16.value = 0;
+    byte2_u result{ Or16(a16, b16) };
+    ASSERT_FALSE(result.value);
 
     // a16=1111'1111'1111'1111
     // b16=0000'0000'0000'0000
-    a16.flip();
+    a16.value = 65535;
     result = Or16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_TRUE(result[i]);
-    }
+    ASSERT_EQ(result.value, 65535);
 
     // a16=1111'1111'1111'1111
     // b16=1111'1111'1111'1111
-    b16.flip();
+    b16.value = 65535;
     result = Or16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_TRUE(result[i]);
-    }
+    ASSERT_EQ(result.value, 65535);
 
     // a16=0000'0000'0000'0000
     // b16=1111'1111'1111'1111
-    a16.flip();
+    a16.value = 0;
     result = Or16(a16, b16);
-    for (int i = 0; i < 16; i++) {
-        ASSERT_TRUE(result[i]);
-    }
+    ASSERT_EQ(result.value, 65535);
 }
 
 TEST_F(LogicGatesTest, Mux16) {
     // a16=1111'1111'1111'1111
     // b16=0000'0000'0000'0000
     // sel=0
-    b16.flip();
-    a16.flip();
-    byte2 result{ Mux16(a16, b16, sel) };
-    ASSERT_EQ(result, a16);
+    b16.value = 0;
+    a16.value = 65535;
+    byte2_u result{ Mux16(a16, b16, sel) };
+    ASSERT_EQ(result.value, a16.value);
 
     // a16=1111'1111'1111'1111
     // b16=0000'0000'0000'0000
     // sel=1
-    sel.flip();
+    sel.value = 1;
     result = Mux16(a16, b16, sel);
-    ASSERT_EQ(result, b16);
+    ASSERT_EQ(result.value, b16.value);
 }
 
 TEST_F(LogicGatesTest, Or8Way) {
     // a8=0000'0000
-    ASSERT_FALSE(Or8Way(a8)[0]);
+    ASSERT_FALSE(Or8Way(a8).value);
 
-    a8[3].flip();
-    ASSERT_TRUE(Or8Way(a8)[0]);
+    a8.byte1_s.val2 = 1;
+    ASSERT_TRUE(Or8Way(a8).value);
 }
 
 TEST_F(LogicGatesTest, Mux4Way16) {
@@ -278,24 +260,24 @@ TEST_F(LogicGatesTest, Mux4Way16) {
     // d16=0000'0000'1111'1111
 
     // sel2=00
-    byte2 result{ Mux4Way16(a16, b16, c16, d16, sel2) };
-    ASSERT_EQ(result, a16);
+    byte2_u result{ Mux4Way16(a16, b16, c16, d16, sel2) };
+    ASSERT_EQ(result.value, a16.value);
 
     // sel2=01
-    sel2.set(0);
+    sel2.bit2_s.val0 = 1;
     result = Mux4Way16(a16, b16, c16, d16, sel2); 
-    ASSERT_EQ(result, b16);
+    ASSERT_EQ(result.value, b16.value);
 
     // sel2=10
-    sel2.set(1);
-    sel2.reset(0);
+    sel2.bit2_s.val0 = 0;
+    sel2.bit2_s.val1 = 1;
     result = Mux4Way16(a16, b16, c16, d16, sel2); 
-    ASSERT_EQ(result, c16);
+    ASSERT_EQ(result.value, c16.value);
     
     // sel2=11
-    sel2.set(0);
+    sel2.bit2_s.val0 = 1;
     result = Mux4Way16(a16, b16, c16, d16, sel2); 
-    ASSERT_EQ(result, d16);
+    ASSERT_EQ(result.value, d16.value);
 }
 
 TEST_F(LogicGatesTest, Mux8Way16) {
@@ -309,128 +291,128 @@ TEST_F(LogicGatesTest, Mux8Way16) {
     // h16=0000'0000'0000'1111
 
     // sel3=000
-    byte2 result{ Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3) };
-    ASSERT_EQ(result, a16);
+    byte2_u result{ Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3) };
+    ASSERT_EQ(result.value, a16.value);
 
     // sel3=001
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, b16);
+    ASSERT_EQ(result.value, b16.value);
 
     // sel3=010
-    sel3.reset(0);
-    sel3.set(1);
+    sel3.bit3_s.val0 = 0;
+    sel3.bit3_s.val1 = 1;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, c16);
+    ASSERT_EQ(result.value, c16.value);
 
     // sel3=011
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, d16);
+    ASSERT_EQ(result.value, d16.value);
 
     // sel3=100
-    sel3.set(2);
-    sel3.reset(1);
-    sel3.reset(0);
+    sel3.bit3_s.val2 = 1;
+    sel3.bit3_s.val1 = 0;
+    sel3.bit3_s.val0 = 0;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, e16);
+    ASSERT_EQ(result.value, e16.value);
 
     // sel3=101
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, f16);
+    ASSERT_EQ(result.value, f16.value);
 
     // sel3=110
-    sel3.set(1);
-    sel3.reset(0);
+    sel3.bit3_s.val1 = 1;
+    sel3.bit3_s.val0 = 0;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, g16);
+    ASSERT_EQ(result.value, g16.value);
 
     // sel3=111
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = Mux8Way16(a16, b16, c16, d16, e16, f16, g16, h16, sel3); 
-    ASSERT_EQ(result, h16);
+    ASSERT_EQ(result.value, h16.value);
 }
 
 TEST_F(LogicGatesTest, DMux4Way) {
     // a=1, sel2=00
-    a.flip();
-    bit4 result{ DMux4Way(a, sel2) }; 
-    bit4 expected{ 0b0001u };
-    ASSERT_EQ(result, expected);
+    a.value = 1;;
+    bit4_u result{ DMux4Way(a, sel2) }; 
+    bit4_u expected{ 1 };
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=01
-    sel2.set(0);
+    sel2.bit2_s.val0 = 1;
     result = DMux4Way(a, sel2); 
-    expected = 0b0010u;
-    ASSERT_EQ(result, expected);
+    expected.value = 2;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=10
-    sel2.set(1);
-    sel2.reset(0);
+    sel2.bit2_s.val1 = 1;
+    sel2.bit2_s.val0 = 0;
     result = DMux4Way(a, sel2); 
-    expected = 0b0100u;
-    ASSERT_EQ(result, expected);
+    expected.value = 4;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=11
-    sel2.set(0);
+    sel2.bit2_s.val0 = 1;
     result = DMux4Way(a, sel2); 
-    expected = 0b1000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 8;
+    ASSERT_EQ(result.value, expected.value);
 }
 
 TEST_F(LogicGatesTest, DMux8Way) {
     // a=1, sel3=000
-    a.flip();
-    byte result{ DMux8Way(a, sel3) }; 
-    byte expected{ 0b0000'0001u };
-    ASSERT_EQ(result, expected);
+    a.value = 1;;
+    byte1_u result{ DMux8Way(a, sel3) }; 
+    byte1_u expected{ 1 };
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel3=001
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = DMux8Way(a, sel3); 
-    expected = 0b0000'0010u;
-    ASSERT_EQ(result, expected);
+    expected.value = 2;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=010
-    sel3.set(1);
-    sel3.reset(0);
+    sel3.bit3_s.val1 = 1;
+    sel3.bit3_s.val0 = 0;
     result = DMux8Way(a, sel3); 
-    expected = 0b0000'0100u;
-    ASSERT_EQ(result, expected);
+    expected.value = 4;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=011
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = DMux8Way(a, sel3); 
-    expected = 0b0000'1000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 8;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=100
-    sel3.set(2);
-    sel3.reset(1);
-    sel3.reset(0);
+    sel3.bit3_s.val2 = 1;
+    sel3.bit3_s.val1 = 0;
+    sel3.bit3_s.val0 = 0;
     result = DMux8Way(a, sel3); 
-    expected = 0b0001'0000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 16;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=101
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = DMux8Way(a, sel3); 
-    expected = 0b0010'0000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 32;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=110
-    sel3.set(1);
-    sel3.reset(0);
+    sel3.bit3_s.val1 = 1;
+    sel3.bit3_s.val0 = 0;
     result = DMux8Way(a, sel3); 
-    expected = 0b0100'0000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 64;
+    ASSERT_EQ(result.value, expected.value);
 
     // a=1, sel=111
-    sel3.set(0);
+    sel3.bit3_s.val0 = 1;
     result = DMux8Way(a, sel3); 
-    expected = 0b1000'0000u;
-    ASSERT_EQ(result, expected);
+    expected.value = 128;
+    ASSERT_EQ(result.value, expected.value);
 }
 
 
